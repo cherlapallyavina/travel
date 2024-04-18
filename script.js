526d97f82e8cc9703d58747dea1850e49e1a0e02
Avina <avina852002@gmail.com>
2024-04-18 11:22:07 +0530

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = 10
+var b=2                                                                                                                           0
+
+
+console.log(a+b)
+
+let c = true
+
+const d ="vinay"
+
+console.log(d)
+console.log(c)
